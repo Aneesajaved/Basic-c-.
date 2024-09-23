914bdde1c6b3124874124ff07b59e53e23fab4cb
Aneesajaved <162286564+Aneesajaved@users.noreply.github.com>
2024-09-23 08:54:00 +0500

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+/*
+int main()
+{
+// cout<< "hello my first code \n";
+// cout<<"hello aneesa";
+
+int _2s = -123;
+cout<< _2s <<endl;
+cout<< sizeof(_2s) <<" bytes" <<endl <<endl;
+
+
+unsigned $vepostine0 = 123665658;
+cout<< $vepostine0 <<endl;
+cout<< sizeof($vepostine0) <<" bytes" <<endl  <<endl;
+
+signed $nagiv_postive_no_bydefult = -1236 ;
+cout<< $nagiv_postive_no_bydefult <<endl;
+cout<< sizeof($nagiv_postive_no_bydefult) <<" bytes" <<endl  <<endl;
+
+char _casting_type = 70;
+cout<< _casting_type <<endl;
+cout<< sizeof(_casting_type) <<" bytes" <<endl <<endl;
+
+int ch = 'a'; // 1byte
+cout<< ch <<endl;
+cout<< sizeof(ch) <<" bytes" <<endl <<endl;
+
+float fl = 1.23; // 4byte
+cout<< fl <<endl;
+cout<< sizeof(fl) <<" bytes" <<endl  <<endl;
+
+double double_flot = 1.2357005923; // 8byte
+cout<< double_flot <<endl;
+cout<< sizeof(double_flot) <<" bytes" <<endl  <<endl;
+
+bool _2bool = 1;//0,1
+cout<< _2bool <<endl;
+cout<< sizeof(_2bool) <<" bytes" <<endl <<endl;
+
+
+long toMuchLongNumber = -1235340; // 8byte
+cout<< toMuchLongNumber <<endl;
+cout<< sizeof(toMuchLongNumber) <<" bytes" <<endl  <<endl;
+
+short toMuchShortNumber = -123; //2byte
+cout<< toMuchShortNumber <<endl;
+cout<< sizeof(toMuchShortNumber) <<" bytes" <<endl  <<endl;
+
+     return 0;
+} 
+*/
+
+// int main(int argc, char const *argv[])
+// {
+//      // get input
+//      int a;
+//      cout << "a :";
+//      cin >> a;
+
+//      int b;
+//      cout << "b :";
+//      cin >> b;
+
+//      int c;
+//      c = a; 
+//      a = b;
+//      b = c;
+
+//      cout << "a : " << a << endl
+//           << "b : " << b << endl;
+
+//      return 0;
+// }
