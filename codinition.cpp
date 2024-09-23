23d6a535c697cfa3f4a7fa3ce6a9a80706152967
Aneesajaved <162286564+Aneesajaved@users.noreply.github.com>
2024-09-23 08:50:22 +0500

    Add files via upload

---
+++ codinition.cpp
@@ -0,0 +1,234 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+  // ---------------------- +ve -ve 0
+  //      int num1;
+  //      cout << " enter the number : ";
+  //      cin >> num1;
+
+  //      if (num1>0)
+  //      {
+  //         cout<< " this number is postion" ;
+  //      }
+
+  //     else if (num1<0)
+  //      {
+  //         cout<< " this number is -ve" ;
+  //      }
+
+  //     else  if (num1==0)
+  //      {
+  //         cout<< " this number is equal 0" ;
+  //      }
+
+  //------------------------------------ marks
+  //      int num1;
+  //      cout << " enter the number : ";
+  //      cin >> num1;
+
+  //      if (num1 > 33)
+  //      {
+
+  //           if (num1 > 80)
+  //           {
+  //                cout << " this number is graysfull marks";
+  //           }
+  //           cout << " this number is pass";
+  //      }
+
+  //      else
+  //      {
+  //           cout << " your fail ";
+  //      }
+
+  // ----------------------------- gf
+  // int num1;
+  // cout << " enter the number : ";
+  // cin >> num1;
+
+  // if (num1 > 70)
+  // {
+  //      cout << " you go nayha shoping";
+  // }
+
+  // else if (num1>  30)
+  // {
+  //      cout << " you go not sopping naha";
+  // }
+
+  // else
+  // {
+  //      cout << "friend ";
+  // }
+
+  // -------------------------------greater then
+  // this sol by nasting statement
+  // int a, b, c;
+  // cin >> a >> b >> c;
+  // if (a > b && a > c)
+  // {
+  //      cout << a;
+  // }
+  // else if (b>a && b>c)
+  // {
+  //      cout << b;
+  // }
+  // else if (c > b && c>a)
+  // {
+  //      cout <<c ;
+  // }
+
+  // -------------------------even and and odd
+  // int number;
+  // cout << " enter the numbr";
+  // cin >> number;
+
+  // if (number % 2 ==0)
+  // {
+  //      cout << " this is even number";
+  // }
+  // else{
+  //       cout << " this is odd number";
+  // }
+
+  // ------------------------------diving case
+  // char _5;
+  // cout << " chaerter enter the : ";
+  // cin >> _5;
+  // switch (_5)
+  // {
+  // case 'a':
+  //      cout << "yes volwe";
+  //      break;
+  // case 'e':
+  //      cout << "yes volwe";
+  //      break;
+  // case 'i':
+  //      cout << "yes volwe";
+  //      break;
+  // case 'o':
+  //      cout << "yes volwe";
+  //      break;
+  // case 'u':
+  //      cout << "yes volwe";
+  //      break;
+
+  // default:
+  // cout<<"not vowle";
+  //      break;
+  // }
+
+  // ------------------------volwe char
+  // we do with siwch case
+  // char a;
+  // cin >> a;
+  // if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u')
+  // {
+  //   cout << "volwe";
+  // }
+
+  // else
+  // {
+  //   cout << "not";
+  // }
+
+  // ----------------------------calulater
+  // float a, b;
+  // cout << " enter the number 2: ";
+  // cin >> a >> b;
+
+  // char op;
+  // cout << " enter the operation : ";
+  // cin >> op;
+
+  // if (op=='+')
+  // {
+  //   cout << a + b << endl;
+  // }
+  //  else if (op=='-')
+  // {
+  //   cout << a - b << endl;
+  // }
+  //  else if (op=='*')
+  // {
+  //   cout << a * b << endl;
+  // }
+  //  else if (op=='/')
+  // {
+  //   cout << a / b << endl;
+  // }
+  //  else
+  // {
+  //   cout << "this case not vail " << endl;
+  // }
+  //  ------------------------------------------------------------
+  // switch (op)
+  // {
+  // case '+':
+  //   cout << a + b<<endl;
+  //   break;
+  // case '-':
+  //   cout << a - b<<endl;
+  //   break;
+  // case '*':
+  //   cout << a * b<<endl;
+  //   break;
+  // case '/':
+  //   cout << a / b<<endl;
+  //   break;
+
+  // default:
+  // cout << "this is not vaild";
+  //   break;
+  // }
+  // --------------------------------------------char button
+  // char button;
+  // cin >> button;
+
+  // if (button == 'a')
+  // {
+  //   cout << "aneesa";
+  // }
+  // else if (button=='b')
+  // {
+  //   cout << "bilo";
+  // }
+  // else{
+  //   cout << "learing";
+  // }
+
+  // switch (button)
+  // {
+  // case 'a':
+  //   cout<<"aneesa";
+  //   break;
+  // case 'b':
+  //   cout<<"bilo";
+  //   break;
+  // case 'c':
+  //   cout<<"cat";
+  //   break;
+
+  // default:
+  // cout<< " i learn more then";
+
+  //   break;
+  // }
+  // ----------------------------------lowercase uppercase
+  char a;
+  cin >> a;
+
+  if (a=97 <= a=122)
+  {
+    cout << " lower case";
+  }
+  else
+  {
+    cout << "not";
+  }
+
+
+
+  return 0;
+}
