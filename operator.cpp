2f4c081cbef34c8a70083f7f2d9991e6486ebfb8
Aneesajaved <162286564+Aneesajaved@users.noreply.github.com>
2024-09-23 09:03:03 +0500

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+// -----------------------------arithmetic operator
+// int main(int argc, char const *argv[])
+// {
+//      int a = 2, b = 6;
+//      cout << a + b << endl;
+//      cout << a - b << endl;
+//      cout << a * b << endl;
+//      cout << a / b << endl;
+//      cout << a % b << endl;
+
+//      return 0;
+// }
+
+// ---------------------increment and decrement
+// int main(int argc, char const *argv[])
+//{
+// int a = 10; // 10
+// int b;      // 11
+// b = ++a; // a=11
+// cout << " a: " << a << endl
+//      << " b: " << b << endl;
+
+// int a = 10; // 11
+// int b;      // 10
+// b = a++; // a=11
+// cout << " a: " << a << endl
+//      << " b: " << b << endl;
+
+// int i = 1, j = 2, k;
+// //  k = i + j + i++ + ++i + ++j;
+// // other value
+// k = i++ - --i + ++i - --j;
+// cout << k << endl;
+
+// return 0;
+//}
+
+//-------------------ralative operator
+
+int main(int argc, char const *argv[])
+{
+
+     // int n;
+     // cout << "n:  ";
+     // cin >> n;
+
+     // if (n > 10)
+     // {
+     //      cout << " more then 10 " << endl;
+     // }
+     // else if (n < 10)
+     // {
+     //      cout << " less then 10 " << endl;
+     // }
+     // else
+     // {
+     //      cout << " equal to 10 " << endl;
+     // }
+
+     // logical operator
+
+     // --------------------bitwis operator
+
+     // int a = 4;
+     // int b =9;
+
+     // int c = a | b; //13
+     //  int c = a & b; // 0
+     //  int c = a ^ b;//13
+     //  int c = ~a;//
+     //  int c = a << 1;//8
+     //  int c = a << 2;//16
+     //  int c = a>>2;//1
+
+     // cout << c << endl;
+
+     // ----------------------condition operator
+     int a =4,b=4;
+     bool flag;
+      a == b ? flag = true : flag = false;
+      cout<< flag;
+
+     return 0;
+}
